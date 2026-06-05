b119f4d4283c8aadb5e53cf2b0c0ea29c7043dd6
Lesha226 <lesha226@yandex.ru>
2026-06-06 02:04:33 +0300

    add template-service

---
+++ template-service/src/test/java/ru/yandex/practicum/mybank/service/template/TemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum.mybank.service.template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
